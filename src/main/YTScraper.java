75b4cb279c788ad3795d74bb26f0c0451acf16f2
VolcanicArts <VolcanicArtsBP@Gmail.com>
2020-07-04 23:17:16 +0100

    First successful scrape of title

--- src/main/YTScraper.java
+++ src/main/YTScraper.java
@@ -6,6 +6,11 @@ import java.net.URISyntaxException;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import org.json.JSONObject;
 
 public class YTScraper {
 	
@@ -29,8 +34,19 @@ public class YTScraper {
 		}
 		
 		if (res == null) return null;
-		
-		System.out.println(res.body());
+		//System.out.println(res.body());
+		String body = (String) res.body();
+		Pattern p = Pattern.compile("\\{};ytplayer.config = (\\{.*?.*\\})");
+		Matcher m = p.matcher(body);
+		if (m.find()) {
+			String found = m.group(1);
+			//System.out.println(found);
+			JSONObject data = new JSONObject(found);
+			JSONObject playerData = new JSONObject(data.getJSONObject("args").getString("player_response"));
+			System.out.println(playerData.getJSONObject("videoDetails").getString("title"));
+		} else {
+			System.out.println(":(");
+		}
 		return null;
 	}
 
