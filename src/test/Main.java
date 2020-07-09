79a9d583c2dcc7272befaf0086624f616633f367
VolcanicArts <VolcanicArtsBP@Gmail.com>
2020-07-09 01:27:52 +0100

    Updated test case to handle new exceptions

--- src/test/Main.java
+++ src/test/Main.java
@@ -5,18 +5,28 @@ import java.net.URISyntaxException;
 
 import com.volcanicarts.ytscraper.api.YTScraper;
 import com.volcanicarts.ytscraper.api.YTVideo;
+import com.volcanicarts.ytscraper.internal.InvalidVideoException;
 
 public class Main {
 
 	public static void main(String[] args) throws URISyntaxException {
 		// A simple test case for using the YTScraper and YTVideo classes
 		URI uri = new URI("https://www.youtube.com/watch?v=dQw4w9WgXcQ");
-		YTVideo video = YTScraper.getYouTubeVideoInfo(uri);
-		System.out.println("ID: " + video.getID());
-		System.out.println("Title: " + video.getTitle());
-		System.out.println("Duration: " + video.getDuration());
-		System.out.println("Category: " + video.getCategory());
-		System.out.println("Uploaded: " + video.getUpload());
+		YTVideo video = null;
+		try {
+			video = YTScraper.getYouTubeVideoInfo(uri);
+		} catch (InvalidVideoException e) {
+			e.printStackTrace();
+		}
+		if (video != null) {
+			System.out.println("ID: " + video.getID());
+			System.out.println("Title: " + video.getTitle());
+			System.out.println("Duration: " + video.getDuration());
+			System.out.println("Category: " + video.getCategory());
+			System.out.println("Uploaded: " + video.getUpload());
+		} else {
+			System.out.println("No data could be found");
+		}
 	}
 
 }
