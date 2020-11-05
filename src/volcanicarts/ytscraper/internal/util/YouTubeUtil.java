b2d64a63dca77eff61621e291713d1784c5e1c33
VolcanicArts <VolcanicArtsBP@Gmail.com>
2020-11-05 18:23:20 +0000

    Change pattern to static final

--- src/volcanicarts/ytscraper/internal/util/YouTubeUtil.java
+++ src/volcanicarts/ytscraper/internal/util/YouTubeUtil.java
@@ -11,14 +11,15 @@ import java.util.regex.Pattern;
  */
 public class YouTubeUtil {
 	
+	private static final String ID_REGEX = "(?<=watch\\?v=|/videos/|embed\\/|youtu.be\\/|\\/v\\/|\\/e\\/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2F|youtu.be%2F|%2Fv%2F)[^#\\&\\?\\n]*";
+	private static final Pattern ID_PATTERN = Pattern.compile(ID_REGEX);
+	
 	/**
 	 * @param URI
 	 * @return A video ID
 	 */
 	public static String videoLinkToID(URI uri) {
-		String pattern = "(?<=watch\\?v=|/videos/|embed\\/|youtu.be\\/|\\/v\\/|\\/e\\/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2F|youtu.be%2F|%2Fv%2F)[^#\\&\\?\\n]*";
-		Pattern r = Pattern.compile(pattern);
-		Matcher m = r.matcher(uri.toString());
+		Matcher m = ID_PATTERN.matcher(uri.toString());
 		if (m.find()) {
 			return m.group(0);
 		} else {
